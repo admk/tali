500b01fd240ded79faf09a896dfabca6baac0543
Xitong Gao <238821+admk@users.noreply.github.com>
2025-05-22 01:19:06 +0800

    Add pyproject stuff

--- tusk/__init__.py
+++ tusk/__init__.py
@@ -1,2 +1,6 @@
-__toolname__ = "tusk"
+__toolname__ = __name__
 __version__ = "0.0.1"
+__author__ = "admk"
+__license__ = "MIT"
+__description__ = ":crystal_ball::sparkles: Weave productivity spells with symbols that conjures order from chaos."
+__url__ = "https://github.com/admk/tusk"
